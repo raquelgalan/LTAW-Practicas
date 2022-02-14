e4b9128813a6bd7137e267e297adf0665bbca876
raquelgalan <raquel.galan.montes@gmail.com>
2022-02-14 12:51:15 +0100

    apuntes

---
+++ P1/tienda.js
@@ -0,0 +1,5 @@
+
+recurso principal index.html
+al menos 3 productos => producto1.html, producto2.html y producto3.html// producto1.css, producto2.css y producto3.css
+
+mejoras ej:añadir recurso _ls (puerta trasera al server) node lee los ficheros que hay en el directorio y los devuelve.
