0d599e8afe155c3a52c0b1e0c5dd3f79b2a40e42
raquelgalan <raquel.galan.montes@gmail.com>
2022-03-07 22:06:05 +0100

    pag ok

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,6 +2,7 @@
 
 //-- Importar los módulos
 const http = require('http'); //-- Módulo http, creación de servidores web
+const url = require('url'); //-- Módulo url, analiza y procesa URLs
 const fs = require('fs'); //-- Módulo fs, acceso al sistema de ficheros
 
 //-- Declarar constantes
@@ -19,6 +20,24 @@ const mime = {
 //-- Mensaje de arranque
 console.log("Arrancando el servidor...");
 
+
+//-- Texto HTML
+const pagina = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>¡Happy Server!</title>
+</head>
+<body style="background-color: lightblue">
+    <h1 style="color: green">HAPPY SERVER!!!</h1>
+</body>
+</html>
+`
+
 //-- Crear el servidor. Por cada petición se imprime mensaje.
 const server = http.createServer((req, res) => {
 
@@ -27,11 +46,17 @@ const server = http.createServer((req, res) => {
 
     //-- Cabecera que indica el tipo de datos del
     //-- cuerpo de la respuesta: Texto plano
-    res.setHeader('Content-Type', 'text/plain');
-    
-    //-- Mensaje del cuerpo
-    res.write("Soy una tienda on-line con mucho arte!\n");
-    
+    //--res.setHeader('Content-Type', 'text/html');
+   
+    //-- Generar respuesta
+    //-- web OK
+    res.statusCode = 200;
+    res.statusMessage = "OK";
+    //-- Cabecera que indica el tipo de datos de la respuesta: Texto html
+    res.setHeader('Content-Type', 'text/html');
+    //-- Mensaje del cuerpo de la pagina
+    res.write(pagina);
+
     //-- Terminar la respuesta y enviarla
     res.end();
 
@@ -44,6 +69,10 @@ server.listen(PUERTO);
 
 //-- Mensaje de inicio del servidor
 console.log("Web artística!. Escuchando en el puerto: " + PUERTO);
+
+
+
+
 
 /* recurso principal index.html
 al menos 3 productos => producto1.html, producto2.html y producto3.html// producto1.css, producto2.css y producto3.css
