c7656af6f57b3c882c6c0da5bed1e3496be63cfe
raquelgalan <raquel.galan.montes@gmail.com>
2022-03-14 21:58:06 +0100

    pagina_error

--- P1/tienda.js
+++ P1/tienda.js
@@ -22,7 +22,7 @@ console.log("Arrancando el servidor...");
 
 
 //-- Texto HTML
-const pagina = `
+const pagina_main = `
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,6 +38,22 @@ const pagina = `
 </html>
 `
 
+//-- Texto HTML de la página de error
+const pagina_error = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi tienda</title>
+</head>
+<body style="background-color: red">
+    <h1 style="color: white">ERROR!!!!</h1>
+</body>
+</html>
+`
+
 //-- Crear el servidor. Por cada petición se imprime mensaje.
 const server = http.createServer((req, res) => {
 
@@ -48,14 +64,34 @@ const server = http.createServer((req, res) => {
     //-- cuerpo de la respuesta: Texto plano
     //--res.setHeader('Content-Type', 'text/html');
    
-    //-- Generar respuesta
+    //-- Generar respuesta por defecto
     //-- web OK
-    res.statusCode = 200;
-    res.statusMessage = "OK";
+    let code = 200;
+    let code_sms = "OK";
     //-- Cabecera que indica el tipo de datos de la respuesta: Texto html
-    res.setHeader('Content-Type', 'text/html');
+    // -- res.setHeader('Content-Type', 'text/html');
     //-- Mensaje del cuerpo de la pagina
-    res.write(pagina);
+    let page = pagina_main;
+
+//-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    if (url.pathname != '/') {
+        code = 404;
+        code_sms = "Not Found";
+        page = pagina_error;
+    }
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_sms y page
+    res.statusCode = code;
+    res.statusMessage = code_sms;
+    res.setHeader('Content-Type','text/html');
+    res.write(page);
 
     //-- Terminar la respuesta y enviarla
     res.end();
