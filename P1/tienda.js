8c8f7a82969bb3df98b21f1ff73b089d710d2eb8
raquelgalan <raquel.galan.montes@gmail.com>
2022-03-20 20:34:16 +0100

    error ok con error.html

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,6 +7,7 @@ const fs = require('fs'); //-- Módulo fs, acceso al sistema de ficheros
 
 //-- Declarar constantes
 const PUERTO = 9090; //-- Puerto a utilizar
+const PAGINA ='tienda.html'; //-- Página web
 
 //-- Declarar valores tipo MIME
 const mime = {
@@ -17,102 +18,62 @@ const mime = {
 
  };
 
-//-- Mensaje de arranque
-console.log("Arrancando el servidor...");
+//-- Crear el sevidor
+const server = http.createServer(function (req, res) {
 
 
-//-- Texto HTML
-const pagina_main = `
+    //-- Crear el objeto URL del mensaje de solitud (req)
+    //-- y coger el recurso (url)
+    let myURL = url.parse(req.url, true);
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>¡Happy Server!</title>
-</head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">HAPPY SERVER!!!</h1>
-</body>
-</html>
-`
-
-//-- Texto HTML de la página de error
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
-
-//-- Crear el servidor. Por cada petición se imprime mensaje.
-const server = http.createServer((req, res) => {
-
-    //-- Indicamos que se ha recibido una petición
-    console.log("Petición recibida!");
+    //-- Ruta de recurso
+    console.log("Recurso recibido: " + myURL.pathname);
 
     //-- Cabecera que indica el tipo de datos del
     //-- cuerpo de la respuesta: Texto plano
-    //--res.setHeader('Content-Type', 'text/html');
-   
-    //-- Generar respuesta por defecto
-    //-- web OK
-    let code = 200;
-    let code_sms = "OK";
-    //-- Cabecera que indica el tipo de datos de la respuesta: Texto html
-    // -- res.setHeader('Content-Type', 'text/html');
-    //-- Mensaje del cuerpo de la pagina
-    let page = pagina_main;
+    res.setHeader('Content-Type', 'text/plain');
 
-//-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
-    const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
+    //-- Cargar en el fichero mi tienda
+    let filename = "";
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_sms = "Not Found";
-        page = pagina_error;
+    //-- Obtenemos el fichero correspondiente.
+    if (myURL.pathname == '/'){
+        filename += "tienda.html";  //-- Abrir página
+    }else{
+        filename += myURL.pathname.substr(1);  //-- Abrir fichero
     }
 
-    //-- Generar la respusta en función de las variables
-    //-- code, code_sms y page
-    res.statusCode = code;
-    res.statusMessage = code_sms;
-    res.setHeader('Content-Type','text/html');
+    //-- Página web html con lectura asíncrona
+    fs.readFile(filename, (error, page) => {
+        if (error){
+            console.log (error.message) 
+            console.log(filename);
+            //filename == "error.html";
+            //console.log(filename);
+            res.writeHead(404, {'Content-Type': mime});
+            //return error.html;
+            
+            //return res.end("404 ERROR");
+            //console.log(err.message);
+
+
+        }else{
+            //-- Petición 200 OK
+            res.writeHead(200, {'Content-Type': mime});
+            console.log("Petición 200 OK");
+            
+      }
+    
     res.write(page);
 
     //-- Terminar la respuesta y enviarla
     res.end();
-
+    });
 });
 
-
-//-- Activar el servidor: ¡Que empiece la fiesta!
+//-- El servidor escucha en el puerto
 server.listen(PUERTO);
 
 
 //-- Mensaje de inicio del servidor
 console.log("Web artística!. Escuchando en el puerto: " + PUERTO);
-
-
-
-
-
-/* recurso principal index.html
-al menos 3 productos => producto1.html, producto2.html y producto3.html// producto1.css, producto2.css y producto3.css
-
-mejoras ej:añadir recurso _ls (puerta trasera al server) node lee los ficheros que hay en el directorio y los devuelve.
-
-*/
