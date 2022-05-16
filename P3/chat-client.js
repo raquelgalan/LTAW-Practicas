b16a841f7b8225c2b07ec12f869fb426856827d8
raquelgalan <raquel.galan.montes@gmail.com>
2022-05-16 23:57:52 +0200

    servidor

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -1,3 +1,5 @@
+/* CLIENTE/S */
+
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
--- P3/chat-server.js
+++ P3/chat-server.js
@@ -1,3 +1,5 @@
+/* SERVIDOR */
+
 //-- Cargar las dependencias
 const socket = require('socket.io');
 const http = require('http');
@@ -15,39 +17,68 @@ const server = http.Server(app);
 //-- Crear el servidor de websockets, asociado al servidor http
 const io = socket(server);
 
+//-- Contador de usuarios
+let Contador = 0;
+
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Test</a></p>');
+  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Chat</a></p>');
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
 //-- biblioteca socket.io para el cliente
 app.use('/', express.static(__dirname +'/'));
 
-//-- El directorio publico contiene ficheros estáticos
-app.use(express.static('public'));
-
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
   
-  console.log('** NUEVA CONEXIÓN **'.yellow);
+    console.log('** NUEVO USUARIO CONECTADO **'.yellow);
+    Contador +=1; //-- Se incrementa contador
 
-  //-- Evento de desconexión
-  socket.on('disconnect', function(){
-    console.log('** CONEXIÓN TERMINADA **'.yellow);
-  });  
+    // Mensaje de Bienvenida al nuevo usuario
+    socket.send('¡Bienvenido!');
 
-  //-- Mensaje recibido: Hacer eco
-  socket.on("message", (msg)=> {
-    console.log("Mensaje Recibido!: " + msg.blue);
+    // Mensaje para todos los usuarios avisando de que se ha conectado un nuevo usuario
+    io.send('Nuevo usuario conectado');
 
-    //-- Hacer eco
-    socket.send(msg);
-  });
+    //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+    socket.on("message", (msg)=> {
+        if (msg.startsWith("/")) {
+            if(msg =='/help'){
+                socket.send("Comandos especiales:" + "<br>" +
+                "/ list: Devolverá el número de usuarios conectados." + "<br>" +
+                " / hello: El servidor nos devolverá el saludo." + "<br>" +
+                " / date: Nos devolverá la fecha");
+            }else if (msg =='/list') {
+                socket.send("Hay " + Contador + " usuario/s" );
+            }else if (msg =='/hello') {
+                socket.send("¡Hola!");
+            }else if (msg =='/date') {
+                let Fecha= new Date();
+                socket.send("La fecha de hoy es: " + Fecha);
+            }else{
+                socket.send("No es un comando especial, en /help tiene más información");
+            }    
+        }else{
+            //-- Reenviarlo a todos los clientes conectados
+            io.send(msg);
+            console.log("Mensaje Recibido!: " + msg.blue);
+        };
+    });
+
+    //-- Evento de desconexión
+    socket.on('disconnect', function(){
+        console.log('** CONEXIÓN TERMINADA **'.yellow);
+        Contador -= 1;
+        // Mensaje para todos los usuarios
+        io.send('Un usuario ha dejado el chat');
+    });
 
 });
+
+
 
 //-- Lanzar el servidor HTTP
 //-- ¡Que empiecen los juegos de los WebSockets!
