2a9da40fa700cf6eb40414e445416497be1b26ea
raquelgalan <raquel.galan.montes@gmail.com>
2022-05-17 13:47:08 +0200

    Mejora: sonido

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -5,19 +5,23 @@ const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
 const send = document.getElementById("send");
 
+//-- Cargar sonido
+let sonido = new Audio('sonido.mp3');
+
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
 //-- Mensaje recibido
 socket.on("message", (msg)=>{
-  display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+    display.innerHTML += '<p style="color:blue">' + msg + '</p>';
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
-  if (msg_entry.value)
-    socket.send(msg_entry.value);
-  
+    if (msg_entry.value)
+        socket.send(msg_entry.value);
+        sonido.play();
+
   //-- Borrar el mensaje actual
   msg_entry.value = "";
 }
